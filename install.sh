fd7e4bb8817a6ca7c092a586c3a0c2d39bf1d869
OsweRRR <blaczeros@gmail.com>
2023-09-21 16:39:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if dpkg-query -W needrestart >/dev/null 2>&1; then
-    sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+     sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 fi
 
 # Get the interface name for the WAN connection using ip a command
@@ -20,38 +20,38 @@ ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 # Installing pptpd
 echo "Installing PPTPD"
-sudo apt-get install pptpd -y
+apt-get install pptpd -y
 
 # edit DNS
 echo "Setting Google DNS"
-sudo echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
-sudo echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
+echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
+echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
 
 # Edit PPTP Configuration
 echo "Editing PPTP Configuration"
 remote="$ppp1"
 remote+="0-200"
-sudo echo "localip $ppp1" >> /etc/pptpd.conf
-sudo echo "remoteip $remote" >> /etc/pptpd.conf
+echo "localip $ppp1" >> /etc/pptpd.conf
+echo "remoteip $remote" >> /etc/pptpd.conf
 
 # Enabling IP forwarding in PPTP server
 echo "Enabling IP forwarding in PPTP server"
-sudo echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-sudo sysctl -p
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
 
 # Tinkering in Firewall
 echo "Tinkering in Firewall"
 if [ -z "$wan" ]
 	then
-		sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE && iptables-save
-		sudo iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
-		$("sudo iptables -I INPUT -s $ip/8 -i ppp0 -j ACCEPT")
-		sudo iptables --append FORWARD --in-interface wlan0 -j ACCEPT
+		iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE && iptables-save
+		iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
+		$(" iptables -I INPUT -s $ip/8 -i ppp0 -j ACCEPT")
+		iptables --append FORWARD --in-interface wlan0 -j ACCEPT
 	else
-		sudo iptables -t nat -A POSTROUTING -o $INTERFACE_NAME -j MASQUERADE && iptables-save
-		sudo iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
-		$("sudo iptables -I INPUT -s $ip/8 -i ppp0 -j ACCEPT")
-		sudo iptables --append FORWARD --in-interface $INTERFACE_NAME -j ACCEPT
+		iptables -t nat -A POSTROUTING -o $INTERFACE_NAME -j MASQUERADE && iptables-save
+		iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
+		$(" iptables -I INPUT -s $ip/8 -i ppp0 -j ACCEPT")
+		iptables --append FORWARD --in-interface $INTERFACE_NAME -j ACCEPT
 fi
 
 clear
@@ -61,9 +61,9 @@ echo "Set username:"
 read username
 echo "Set Password:"
 read password
-sudo echo "$username * $password *" >> /etc/ppp/chap-secrets
+echo "$username * $password *" >> /etc/ppp/chap-secrets
 
 # Restarting Service 
-sudo service pptpd restart
+service pptpd restart
 
 echo "All done!"
